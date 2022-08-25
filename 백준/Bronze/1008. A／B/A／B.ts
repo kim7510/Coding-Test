7cd8a67260a41b3d6ea8028cd98751e92f553cd5
SeoYeon <95194858+kim7510@users.noreply.github.com>
2022-08-25 18:09:29 +0900

    [Bronze V] Title: A/B, Time: 148 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.ts
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split(" ");
+const A:number = parseInt(input[0]);
+const B:number = parseInt(input[1]);
+console.log(A / B);
